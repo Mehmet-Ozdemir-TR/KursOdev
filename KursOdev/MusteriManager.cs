0c9434a7b795a73c0bb1f1b0b0a3ec51230f957e
10mehmet33 <47495341+10mehmet33@users.noreply.github.com>
2021-01-19 21:41:55 +0300

    Add project files.

---
+++ KursOdev/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursOdev
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri yeni)
+        {
+            Console.WriteLine(yeni.Adi + " " + yeni.SoyAd + " :Bankamıza Hoşgeldiniz !");
+        }
+        public void MusteriSil(Musteri silme)
+        {
+            Console.WriteLine(silme.Adi + " " + silme.SoyAd + " :Bankamızdan ayrıldınız !");
+        }
+        public void Listeleme(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("*****************************************");
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.SoyAd);
+                Console.WriteLine(musteri.Id);
+                Console.WriteLine("*****************************************");
+            }
+        }
+    }
+}
---
+++ KursOdev/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KursOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Mehmet";
+            musteri1.SoyAd = "Özdemir";
+            musteri1.Id = 1456;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Yoda";
+            musteri2.SoyAd = "Kalisya";
+            musteri2.Id = 1796;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Süper";
+            musteri3.SoyAd = "Man";
+            musteri3.Id = 25656;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+           
+
+            MusteriManager musteriManger = new MusteriManager();
+            musteriManger.Listeleme(musteriler);
+            musteriManger.MusteriEkleme(musteri2);
+            musteriManger.MusteriSil(musteri3);
+        }
+    }
+}
