a098a31a922375174017338401d02e2d7f13e175
10mehmet33 <47495341+10mehmet33@users.noreply.github.com>
2021-01-23 19:20:07 +0300

    23 ocak cumartesi Son Ödev

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<Key,Value>
+    {
+        Key[] keys; // Key tipinde keys adlı boş bir dizi oluşturduk.
+        Value[] values; // Value tipinde values adlı boş bir dizi oluşturduk.
+
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempArrayK = keys; // dizi boyutunu artırınca elimizdeki elemanlar gitmesin diye referansımızı tuttuk.
+            keys = new Key[keys.Length + 1];
+
+            Value[] tempArrayV = values;// dizi boyutunu artırınca elimizdeki elemanlar gitmesin diye referansımızı tuttuk.
+            values = new Value[values.Length + 1];
+
+            for (int i = 0; i < tempArrayK.Length; i++)
+            {
+                keys[i] = tempArrayK[i];
+            }
+            keys[keys.Length - 1] = key; // verilen değeri keys dizisinin sonuna ekledik.
+
+            for (int i = 0; i < tempArrayV.Length; i++)
+            {
+                values[i] = tempArrayV[i];
+            }
+            values[values.Length - 1] = value; // verilen değeri values dizisinin sonuna ekledik.
+
+            Console.WriteLine(key +" " + value + " :Başarıyla Eklendi!");
+
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(12, "Mehmet");
+            myDictionary.Add(16 , "Saliha");
+        }
+    }
+}
